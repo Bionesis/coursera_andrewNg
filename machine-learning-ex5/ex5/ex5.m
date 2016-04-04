a72b273c13765fb64d33b0da9e98aec16ef8c1b5
nicolasrivollet <nicolas.rivollet@gmail.com>
2016-04-04 12:28:34 +0200

    ex5 / lambda = 3 optimum

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -158,7 +158,7 @@ fprintf('  %f  \n', X_poly(1, :));
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 1;
+lambda = 3;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
